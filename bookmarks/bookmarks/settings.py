a72e74cc3f7832e7433c580162f53f377476f77c
Andrey Plekhanov <ierehon91@gmail.com>
2023-05-26 11:30:56 +0300

    При социальной авторизации теперь создаётся объект профиля

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -152,3 +152,16 @@ SOCIAL_AUTH_TWITTER_SECRET = SocialAuthorizationsSettings.SOCIAL_AUTH_TWITTER_SE
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = SocialAuthorizationsSettings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY  # ИД клиента Google
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = SocialAuthorizationsSettings.SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET  # Секрет клиента Google
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authenticate.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
