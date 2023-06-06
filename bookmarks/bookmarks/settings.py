96fb5e83343d8149c534735c467a3a490209ad85
Andrey Plekhanov <ierehon91@gmail.com>
2023-06-06 11:31:12 +0300

    Добавлены представления и маршруты для списка пользователей и детальной информации о пользователе. В settings.py добавлена настройка для динамического формирования url модели User Созданы шаблоны для списка пользователей и детальной информации о пользователе.

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 from bookmarks import SocialAuthorizationsSettings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -171,3 +172,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
